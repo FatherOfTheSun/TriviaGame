0618e7b63b258ee6d24f4c3e91336cce7fb7405c
Zeus <reeseborchersmsu@gmail.com>
2019-07-06 23:42:59 -0500

    4

--- assests/javascript/app.js
+++ assests/javascript/app.js
@@ -1,151 +1,156 @@
+var myQuestions = [
+    {
+        question: "Which dogs were bred to kill Bulls for sport?",
 
+        answers: {
+            a: 'German Shepard',
+            b: 'Pitbull',
+            c: 'Cane Corso',
+            d: 'Bulldog',
+            e: 'B and D'
+        },
+        correctAnswer: 'e'
+    },
+    {
+        question: "Which dog were bred to hunt lions?",
+        answers: {
+            a: 'Rhodesian Ridgeback',
+            b: 'German Shepard',
+            c: 'Austtalian Shepard',
+            d: 'Cane Corso',
+            e: 'None of the Above'
+        },
+        correctAnswer: 'a'
+    },
+    {
+        question: "Whick dog were bred to hunt Badger?",
+        answers: {
+            a: 'Australian Shepard',
+            b: 'Husky',
+            c: 'Dachshud',
+            d: 'Chihuahua',
+            e: 'Dalmatian'
+        },
+        correctAnswer: 'c'
+    },
+    {
+        question: "Which dogs were bred for hearding livestock such as sheep?",
+        answers: {
+            a: 'German Shepard',
+            b: 'Australian Shepard',
+            c: 'Boarder Collies',
+            d: 'Corgi',
+            e: 'All of the Above'
+        },
+        correctAnswer: 'e'
+    },
+    {
+        question: "Which dog were bred for mountain rescue?",
+        answers: {
+            a: 'St. Bernard',
+            b: 'Husky',
+            c: 'German Shepard',
+            d: 'Alaskan Malamute',
+            e: 'Rhodesian Ridgeback'
+        },
+        correctAnswer: 'a'
+    },
+    {
+        question: "Which dogs were bred for bear hunting?",
+        answers: {
+            a: 'Caucasian Shepard Dog',
+            b: 'Karelian Bear Dog',
+            c: 'Tibetan Mastiff',
+            d: 'Kangal Shepard Dog',
+            e: 'All of the Above'
+        },
+        correctAnswer: 'e'
+    }
+];
+var quizContainer = document.getElementById('quiz');
+var resultsContainer = document.getElementById('results');
+var submitButton = document.getElementById('submit');
 
-
+generateQuiz(myQuestions, quizContainer, resultsContainer, submitButton);
 
 function generateQuiz(questions, quizContainer, resultsContainer, submitButton) {
 
-    //array of question with answers
-    var myQuestions = [
-        {
-            question: "Which dogs were bred to kill Bulls for sport?",
-
-            answers: {
-                a: 'German Shepard',
-                b: 'Pitbull',
-                c: 'Cane Corso',
-                d: 'Bulldog',
-                e: 'B and D'
-            },
-            correctAnswer: 'e'
-        },
-        {
-            question: "Which dog were bred to hunt lions?",
-            answers: {
-                a: 'Rhodesian Ridgeback',
-                b: 'German Shepard',
-                c: 'Austtalian Shepard',
-                d: 'Cane Corso',
-                e: 'None of the Above'
-            },
-            correctAnswer: 'a'
-        },
-        {
-            question: "Whick dog were bred to hunt Badger?",
-            answers: {
-                a: 'Australian Shepard',
-                b: 'Husky',
-                c: 'Dachshud',
-                d: 'Chihuahua',
-                e: 'Dalmatian'
-            },
-            correctAnswer: 'c'
-        },
-        {
-            question: "Which dogs were bred for hearding livestock such as sheep?",
-            answers: {
-                a: 'German Shepard',
-                b: 'Australian Shepard',
-                c: 'Boarder Collies',
-                d: 'Corgi',
-                e: 'All of the Above'
-            },
-            correctAnswer: 'e'
-        },
-        {
-            question: "Which dog were bred for mountain rescue?",
-            answers: {
-                a: 'St. Bernard',
-                b: 'Husky',
-                c: 'German Shepard',
-                d: 'Alaskan Malamute',
-                e: 'Rhodesian Ridgeback'
-            },
-            correctAnswer: 'a'
-        },
-        {
-            question: "Which dogs were bred for bear hunting?",
-            answers: {
-                a: 'Caucasian Shepard Dog',
-                b: 'Karelian Bear Dog',
-                c: 'Tibetan Mastiff',
-                d: 'Kangal Shepard Dog',
-                e: 'All of the Above'
-            },
-            correctAnswer: 'e'
-        }
-    ];
-
-    function showQuestion(questions, quizContainer) {
-
+    function showQuestions(questions, quizContainer) {
+        // we'll need a place to store the output and the answer choices
         var output = [];
         var answers;
 
+        // for each question...
         for (var i = 0; i < questions.length; i++) {
 
+            // first reset the list of answers
             answers = [];
 
+            // for each available answer...
             for (letter in questions[i].answers) {
 
-                // question and output
+                // ...add an html radio button
                 answers.push(
                     '<label>'
-                    + '<input type= "radio" name="question' + i + '"value="' + letter + '">'
-                    + letter + ':'
+                    + '<input type="radio" name="question' + i + '" value="' + letter + '">'
+                    + letter + ': '
                     + questions[i].answers[letter]
                     + '</label>'
                 );
             }
 
-
-
+            // add this question and its answers to the output
             output.push(
                 '<div class="question">' + questions[i].question + '</div>'
-                + '<div class="answers">' + answers.join('') + '<div>'
+                + '<div class="answers">' + answers.join('') + '</div>'
             );
         }
-        //finally combine our output
+
+        // finally combine our output list into one string of html and put it on the page
         quizContainer.innerHTML = output.join('');
     }
 
-    showQuestion(questions, quizContainer);
 
     function showResults(questions, quizContainer, resultsContainer) {
 
-        // get answer from quiz
+        // gather answer containers from our quiz
         var answerContainers = quizContainer.querySelectorAll('.answers');
 
-        //keeps track of answer
-
+        // keep track of user's answers
         var userAnswer = '';
         var numCorrect = 0;
 
+        // for each question...
         for (var i = 0; i < questions.length; i++) {
 
+            // find selected answer
             userAnswer = (answerContainers[i].querySelector('input[name=question' + i + ']:checked') || {}).value;
 
+            // if answer is correct
             if (userAnswer === questions[i].correctAnswer) {
-
+                // add to the number of correct answers
                 numCorrect++;
 
-                answerContainers[i].style.color = 'green';
-
+                // color the answers green
+                answerContainers[i].style.color = 'lightgreen';
             }
+            // if answer is wrong or blank
             else {
-                answerContainers[i].style.color = 'blue';
-
+                // color the answers red
+                answerContainers[i].style.color = 'red';
             }
         }
-        resultsContainer.innerHTML = numCorrect + 'out of' + questions.length;
 
+        // show number of correct answers out of total
+        resultsContainer.innerHTML = numCorrect + ' out of ' + questions.length;
     }
-    //questions
 
-    //submit button on click 
+    // show questions right away
+    showQuestions(questions, quizContainer);
+
+    // on submit, show results
     submitButton.onclick = function () {
         showResults(questions, quizContainer, resultsContainer);
     }
-    var quizContainer = document.getElementById('quiz');
-    var resultsContainer = document.getElementById('results');
-    var submitButton = document.getElementById('submit');
-    generateQuiz(myQuestions, quizContainer, resultsContainer, submitButton);
+
 }
